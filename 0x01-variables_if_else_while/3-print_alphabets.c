a709a50b9547a9e2c9b630e56a863c069aeaad6d
shortcut45 <luckymbani@gmail.com>
2022-10-16 12:53:04 -0700

    if and else with while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,16 @@
  * follwed by a new line
  *return: Always 0 (Success)
  */
- 
+
 int main(void)
 {
 	char ch, CH;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	
+
 	for (CH = 'A'; CH <= 'Z'; CH++)
 	{
 		putchar(CH);

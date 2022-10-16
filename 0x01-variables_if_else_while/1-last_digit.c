7e4e5f01ab8e7c4d7e4b335cf2327f8e98562664
shortcut45 <luckymbani@gmail.com>
2022-10-16 12:01:30 -0700

    if and else with while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,24 +5,24 @@
  * main - assigns a random number to n
  * executes it
  * print the last digit of the number stored in n
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n% 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 	{
-	printf("and is greater than 5\n"); 
+	printf("and is greater than 5\n");
 	}
 	else if (n == 0)
 	{
 	printf("and is 0\n");
 	}
-	else if (n % 10 < 6 && n % 10 !=0)
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
 	printf("and is less than 6 and not 0\n");
 	}

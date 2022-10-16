f57c806fecc5a7b06e7e2dec2a6f6b108f7107b8
shortcut45 <luckymbani@gmail.com>
2022-10-16 12:48:20 -0700

    if and else with while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet is lowercase and uppercase
+ * follwed by a new line
+ *return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+	char ch, CH;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}

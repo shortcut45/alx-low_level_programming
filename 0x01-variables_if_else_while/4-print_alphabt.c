ce693036387a9362216d8425a62c601ea0d12937
shortcut45 <luckymbani@gmail.com>
2022-10-17 01:30:32 -0700

    if and else with while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
@@ -19,6 +19,6 @@ int main(void)
 		}
 		ch++;
 	}
-	 putchar('\n');
-	 return (0);
+	putchar('\n');
+	return (0);
 }

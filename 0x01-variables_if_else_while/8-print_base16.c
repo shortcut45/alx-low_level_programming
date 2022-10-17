18cd3f56b413084b7262b48d712b3f7cb9830c5e
shortcut45 <luckymbani@gmail.com>
2022-10-17 02:57:15 -0700

    if and else with while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/** main - prints all the numbers of base 16 in lowercase,
+/**
+ * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: 0 (Success)
  */

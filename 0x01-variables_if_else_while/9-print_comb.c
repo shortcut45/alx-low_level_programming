e064a10c0d08323a7bd996cb630861d272929c67
shortcut45 <luckymbani@gmail.com>
2022-10-17 03:31:44 -0700

    if and else with while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * prints all possible combinations of single-digit numbers
+ * main - prints all possible combinations of single-digit numbers
  * Return: 0 (Success)
  */
 
@@ -12,10 +12,11 @@ int main(void)
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n != 57){
+		if (n != 57)
+		{
 			putchar(',');
 			putchar(' ');
-			}
+		}
 	}
 	putchar('\n');
 	return (0);

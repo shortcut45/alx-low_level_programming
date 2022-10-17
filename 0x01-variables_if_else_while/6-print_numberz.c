6fbcc13c2058f8f7d2469afc8b86f6aee28bfb02
shortcut45 <luckymbani@gmail.com>
2022-10-17 02:33:27 -0700

    if and else with while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10
- * starting from 0,
- * followed by a new line
- * Return (0) (Success)
+ * main - prints all single degit numbers of base 10
+ * starting from 0, followed by a new line
+ * Return 0 (Success)
  */
  
 int main(void)

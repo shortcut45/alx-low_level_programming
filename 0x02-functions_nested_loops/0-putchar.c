27392a923ce8395251fe37aa22c737b661a92ec8
shortcut45 <luckymbani@gmail.com>
2022-10-19 03:05:04 -0700

    while loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#define "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,6 +9,19 @@
 
 int main(void)
 {
-	printf("_putchar");
+	char ch[8];
+	ch[0] = 95;
+	ch[1] = 112;
+	ch[2] = 117;
+	ch[3] = 116;
+	ch[4] = 99;
+	ch[5] = 104;
+	ch[6] = 97;
+	ch[7] = 114; 
+	
+	for(int i = 0; i < 8; i++)
+		putchar(ch[i]);
+		
+	putchar('\n');
 	return (0);
 }

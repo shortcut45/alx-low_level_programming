f160ff83e968d833e16d06ed8a8677cc533f98d8
shortcut45 <luckymbani@gmail.com>
2022-10-19 10:36:15 -0700

    while loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- * Return: 0 (Success)
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: void
  */
 
 void print_alphabet(void)

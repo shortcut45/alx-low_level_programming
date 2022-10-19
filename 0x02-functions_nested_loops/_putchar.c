093ffd2c4a0ce12e30389ecc5f5f04ad7f60977d
shortcut45 <luckymbani@gmail.com>
2022-10-19 12:03:42 -0700

    while loops

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,7 +4,7 @@
  * @c; The characterto print
  *
  * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {

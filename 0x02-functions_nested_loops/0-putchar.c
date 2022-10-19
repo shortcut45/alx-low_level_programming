bceb256d19e8099fa0f70aaf9321f69457dca4a5
shortcut45 <luckymbani@gmail.com>
2022-10-19 02:12:37 -0700

    while loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,19 +9,6 @@
 
 int main(void)
 {
-	char ch[8];
-	ch[0] = 95;
-	ch[1] = 112;
-	ch[2] = 117;
-	ch[3] = 116;
-	ch[4] = 99;
-	ch[5] = 104;
-	ch[6] = 97;
-	ch[7] = 114;
-	
-	for(int i = 0; i < 8; i++)
-		putchar(ch[i]);
-		
-	putchar('\n');
+	printf("_putchar");
 	return (0);
 }

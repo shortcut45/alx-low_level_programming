26e915aca7a3332d9e702e8778d802f3bc81f74c
shortcut45 <luckymbani@gmail.com>
2022-10-20 14:30:14 -0700

    while loops

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
+#include <stdio.h>
 
 void positive_or_negative(int 1);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-
 
 #endif /* MAIN_H */

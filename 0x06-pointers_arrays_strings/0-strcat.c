c4f3b5e7583e931ce845fc20a9aa32e7a4cbdf4d
shortcut45 <luckymbani@gmail.com>
2022-10-27 15:53:42 -0700

    while loops

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char s1[98] = "Hello";
-	char s2[] = "World!";
-	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
+	strcat(dest, src);
 }

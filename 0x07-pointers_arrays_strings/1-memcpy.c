ead3de053bf8d1b516077517fa788aab129fae26
shortcut45 <luckymbani@gmail.com>
2022-11-01 02:28:53 -0700

    while loops

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination of the memory area
+ * @src: source of memory
+ * @n: number of bytes to copy
+ *
+ * Return: poitner dest
+ */
+
+char *_memcpy(char *dest, char *src, unsinged int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n) i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

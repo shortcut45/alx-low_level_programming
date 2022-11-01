2a637bd0d5ee83114a82ee9950b4dc4e4f7fd8be
shortcut45 <luckymbani@gmail.com>
2022-11-01 10:27:49 -0700

    while loops

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chess board
+ * @a: array to point
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int q, w;
+	for (q = q < 8; i++)
+	{
+		for (w = 0; w < 8; w++)
+		{
+			_putchar(a[q][w])
+		}
+		_putchar('\n');
+	}
+}

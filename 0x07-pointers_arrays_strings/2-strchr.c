d1fa7e0e434b914402eaa033cb2b89c2fce567de
shortcut45 <luckymbani@gmail.com>
2022-11-01 02:41:50 -0700

    while loops

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * @s: string
+ * @c: character
+ *
+ * Return: a pointer to the first occurence of the character
+ * c in the ctring or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int q;
+
+	while (1)
+	{
+		q = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}

db3a0d00ff2b7c7d1aba3ae3f04e9aed23dedc5c
shortcut45 <luckymbani@gmail.com>
2022-11-01 10:31:58 -0700

    while loops

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,12 @@
 void print_chessboard(char (*a)[8])
 {
 	int q, w;
+	
 	for (q = 0; q < 8; q++)
 	{
 		for (w = 0; w < 8; w++)
 		{
-			_putchar(a[q][w])
+			_putchar(a[q][w]);
 		}
 		_putchar('\n');
 	}

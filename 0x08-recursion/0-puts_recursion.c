f0f1c353324b8b7667e823384dd00e21b708197b
shortcut45 <luckymbani@gmail.com>
2022-11-02 10:44:38 -0700

    while loops

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,12 +11,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*(s + 0));
-		_puts_recursion((s + 1));
+		putchar(*(s + 0));
+		puts_recursion((s + 1));
 	}
 	else
 	{
-		_putchar('\n');
+		putchar('\n');
 		return;
 	}
 }

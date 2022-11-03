2c188f3f837dc569020af3b32133cd59942ea862
shortcut45 <luckymbani@gmail.com>
2022-11-03 10:45:22 -0700

    while loops

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and
+ * 0 if not
+ * @s: string
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int i = 0;
+
+	int h = strlen(s) - 1;
+
+	while (h > i)
+	{
+		if (s[i++] != s[h--])
+		{
+			printf("%s is not a palindrome", s);
+			return (0);
+		}
+		else
+		{
+			printf("%s is a palindrome", s);
+			return (1);
+		}
+	}
+}

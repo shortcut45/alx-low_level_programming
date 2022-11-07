7c03a41a5ca9121c7fa431bebc6edc807786dfb8
shortcut45 <luckymbani@gmail.com>
2022-11-07 04:01:13 -0800

    while loops

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: argument
+ * Return: argument
+ */
+
+char *_strdup(char *str)
+{
+	int size;
+	static char *dup;
+	char *len;
+
+	 if (str == NULL)
+	 {
+	 	return (NULL);
+	 }
+
+	 for (size = 0; str[size]; size++)
+	 {
+	 	dup++;
+	 }
+	 dup = (char *) malloc(sizeof(char) * size + 1);
+
+	 if (dup == NULL)
+	 {
+	 	return((char *) NULL);
+	 }
+
+	 len = dup;
+	 while (*str)
+	 {
+	 	*len = *str;
+		len++;
+		str++;
+	 }
+	 *len = '\0';
+
+	 return (dup);
+}

548fc0df67cc36076e24b000238e60b130185c05
shortcut45 <luckymbani@gmail.com>
2022-11-07 13:13:53 -0800

    mallocs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 		{
 			grid[i] = (int *) malloc(sizeof(int) * width);
-			
+
 			if (grid[i] != NULL)
 			{
 				for (j = 0; j < width; j++)
@@ -42,7 +42,7 @@ int **alloc_grid(int width, int height)
 				return (NULL);
 			}
 		}
-		return(grid);
+		return (grid);
 	}
 	else
 		return (NULL);

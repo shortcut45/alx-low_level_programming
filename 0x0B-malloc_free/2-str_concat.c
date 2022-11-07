9edad817d05b0a4ac0989cdbb5d916b8cba71a8b
shortcut45 <luckymbani@gmail.com>
2022-11-07 04:18:55 -0800

    mallocs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,9 +35,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i]; i = i + 1)
 	{
-		concat[x++] = s1[i];
+		concat[x++]  s1[i];
 	}
-	
+
 	for (i = 0; s2[i]; i = i + 1)
 	{
 		concat[x++] = s2[i];

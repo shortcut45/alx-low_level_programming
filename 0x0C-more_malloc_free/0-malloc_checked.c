e47b0bd8a9220110c4bd1912d10f4e0b6bd81d4a
shortcut45 <luckymbani@gmail.com>
2022-11-10 09:31:55 -0800

    while loops

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - alloctates memory using malloc
+ * @b: int size of memory
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}

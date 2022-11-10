12ef85c9f69efc4bfccd0dc3128fff15700f6ecd
shortcut45 <luckymbani@gmail.com>
2022-11-10 09:47:43 -0800

    while loops

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmeb - number of an array
+ * @size: size of array
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	
+	c = arr;
+	for (i = 0; i < nmemb * size; i++)
+	{
+		c[i] = 0;
+	}
+	return (arr);
+}

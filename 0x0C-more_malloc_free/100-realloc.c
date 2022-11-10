4e8b110268bfe80d7797db2572df843fbec053b9
shortcut45 <luckymbani@gmail.com>
2022-11-10 10:08:45 -0800

    while loops

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory blocks using malloc and free
+ * @ptr: pointer
+ * @old_size: size to be located
+ * @new_size: new size
+ * Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr;)
+	}
+
+	free(ptr);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
+}

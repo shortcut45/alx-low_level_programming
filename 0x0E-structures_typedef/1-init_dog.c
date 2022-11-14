2888b5af2540544899e2129dd7139818248b9721
shortcut45 <luckymbani@gmail.com>
2022-11-14 12:38:46 -0800

    while loops

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_og - initializes a variable of tyhpe struct dog
+ * @d: dog's name
+ * @age: dog's age
+ * @owner: dog'd owner
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

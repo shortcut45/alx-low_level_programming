cbb6a78cf7a29eb07cf9fe147559972da6d200fd
shortcut45 <luckymbani@gmail.com>
2022-11-17 11:43:01 -0800

    while loops

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,9 +3,9 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - returns thr sum of all its parameters
+ * sum_them_all - returns the sum of all its parameters
  * @n: number of parameter
- * @... - other parameter
+ * @...: other parameters
  * Return: sum of all parameters
  */
 
@@ -13,19 +13,18 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	int sum = 0;
-	va_start(args, n);
 	unsigned int i;
 
+	va_start(args, n);
+
 	if (n == 0)
-	{
 		return (0);
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
 	va_end(args);
-	
+
 	return (sum);
 }

0a9752e4178415fc78fe5a4f6ec3033de311365e
shortcut45 <luckymbani@gmail.com>
2022-11-17 11:46:04 -0800

    while loops

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: an input string to be printed between numbers
+ * @n: number of parameter
+ * @...: other parameters
+ * Return: 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int i;
+	char *s;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+
+		if (s == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", s);
+
+		if (separator == NULL)
+			continue;
+
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
